5b1bfe91e176bf77d70a1d6e668e5249a7950b97
Stanley Hidalgo <stanleyhidalgo@gmail.com>
2023-06-26 23:45:55 -0400

    CORS added

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-        <h3>Admin Dashboard</h3>
+        <h4>Admin Dashboard</h4>
         <p>Esta ruta debe ser privada</p>
     </>
   )
